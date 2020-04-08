2cd41fe10379dcb2d1618f8d61e35b25bda45c16
Jesus Aranda <aranda.jm@icloud.com>
2020-04-08 11:46:51 -0300

    Creating heroe component and redirect to it from other

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
 })
 export class HeroesComponent implements OnInit {
   heroes: Heroe[];
-  constructor(private _heroesService: HeroesService) {
+  constructor(private _heroesService: HeroesService, private _router: Router) {
     this.heroes = [];
   }
   ngOnInit() {
     this.heroes = this._heroesService.getHeroes();
     console.log(this.heroes);
   }
+
+  // Alternative to go to ohter route!
+  // goToHeroe(idx: number) {
+  //   this._router.navigate(['/heroe', idx]);
+  // }
 }
