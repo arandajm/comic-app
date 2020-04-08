3c4f610807167efc8e6edef5dce4f71eddafc9b3
Jesus Aranda <aranda.jm@icloud.com>
2020-04-08 16:17:51 -0300

    Showing an alert message when there arent results to show

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from '../../services/heroes.service';
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search',
