a0260105d6a993704418a46e7f478fa41ccb96fe
Jesus Aranda <aranda.jm@icloud.com>
2020-04-07 23:23:43 -0300

    using routerLink to go to any route  and routerLinkActive to set css classes when the route is active

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
 
 const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'heroes', component: HeroesComponent },
   { path: '**', redirectTo: 'home', pathMatch: 'full' },
   // { path: 'hero/:id',      component: HeroDetailComponent },
   // {
